dc7757adbcda192ca8f17a4945c383f96a7fd057
zhanghao <2538022915@qq.com>
2024-03-25 22:49:22 +0800

    更新了clear函数

--- Py/crawlers/bilbil/TitleClear.py
+++ Py/crawlers/bilbil/TitleClear.py
@@ -1,11 +1,19 @@
-import re as r
+import re 
 import pandas as pd
 
-data = pd.read_excel("b站抗日主题视频信息.xlsx").iloc[:, 4].tolist()
-rstr = "(<.*?>)"
-i = 0
-for content in data:
-    data[i] = r.replace(rstr, '')
-    i+=1
+# 读取原始 Excel 表格
+data = pd.read_excel("b站心理健康主题视频信息.xlsx")
 
-print(data)
+# 提取需要清理的列数据，并进行清理
+column_to_clean = data.iloc[:, 4].tolist()
+rstr = "<[^>]+>"
+cleaned_column = []
+for content in column_to_clean:
+    cleaned_content = re.sub(rstr, '', str(content))
+    cleaned_column.append(cleaned_content)
+
+# 将清理后的数据替换回原 DataFrame 中相应的列
+data.iloc[:, 4] = cleaned_column
+
+# 保存修改后的 DataFrame 回原 Excel 表格
+data.to_excel("b站心理健康主题视频信息_cleaned.xlsx", index=False)
